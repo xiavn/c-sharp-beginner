029b4d2cf5385bf17882de3c2757d28d5ad18171
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-24 16:51:36 +0000

    Comment exercises

--- Start/Overview/Comments/Program.cs
+++ Start/Overview/Comments/Program.cs
@@ -6,11 +6,21 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// <returns>
+        /// No return value
+        /// </returns>
         static void Main(string[] args)
         {
-
+            // Single line
+            // Another single
             Console.WriteLine("Hello World!");
-
+            /* Multi line comment
+            that spans multiple lines
+            over a single comment */
         }
     }
 }
