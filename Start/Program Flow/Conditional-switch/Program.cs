9e59efe711f0c0d85340aeeb19d09bacec1a346d
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-24 17:14:25 +0000

    Switch examples

--- Start/Program Flow/Conditional-switch/Program.cs
+++ Start/Program Flow/Conditional-switch/Program.cs
@@ -6,10 +6,25 @@ namespace ConditionalOps
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 59;
 
             // TODO: The switch statement
-
+            switch (theVal) {
+                case 50:
+                    Console.WriteLine("theVal is 50");
+                    break;
+                case 51:
+                    Console.WriteLine("theVal is 51");
+                    break;
+                case 52:
+                case 53:
+                case 54:
+                    Console.WriteLine("theVal is between 52 and 54");
+                    break;
+                default:
+                    Console.WriteLine("theVal is something else");
+                    break;
+            }
         }
     }
 }
