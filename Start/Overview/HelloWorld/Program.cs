343f0d95fca50ccf7db1f85c21fe279e152933d0
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-24 16:05:48 +0000

    Initial commit

---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
---
+++ Start/Overview/VarsAndData/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace VarsAndData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare some basic value type variables
+            int i = 10;
+            float f = 2.0f;
+            decimal d = 10.0m;
+            bool b = true;
+            char c = 'c';
+
+            // Declare a string - it's a collection of characters
+            string str = "a string";
+
+            // Declare an implicit variable
+            var x = 10;
+            var z = "Hello!";
+
+            // TODO: Declare an array of values
+            int[] vals = new int[5];
+            string[] strs = {"one", "two", "three"};
+
+            // TODO: Print the values using a Formatting String
+            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}", i,f,d,b,c,str,x,z);
+
+            // TODO: "null" means "no value"
+            object obj = null;
+            Console.WriteLine(obj);
+
+            // TODO: Implicit conversion between types
+            long bignum;
+            bignum = i;
+
+            // TODO: Explicit conversions
+            float i_f = (float)i;
+            Console.WriteLine("{0}",i_f);
+
+            int f_i = (int)f;
+            Console.WriteLine("{0}",f_i);
+        }
+    }
+}
