f85b93c5f29edf798ed6437da475ebeb0003c715
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-24 16:42:46 +0000

    Operator examples

--- Start/Overview/Operators/Program.cs
+++ Start/Overview/Operators/Program.cs
@@ -12,30 +12,40 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
 
             // TODO: Operators can be shorthand: a = a + b
+            a += b;
+            Console.WriteLine(a);
 
 
             // TODO: Logical operators &&, ||
-            // Console.WriteLine("----- Logic Operators -----");
-
+            Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
 
             // null-coalescing operators
-            // string str = null;
+            string str = "hello";
             // TODO: the ?? operator uses left operand if not null, or right one if it is
-
+            Console.WriteLine(str ?? "Unknown String");
             
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
             //    variable = somevalue;
             // }
-
+            str ??= "New String";
+            Console.WriteLine(str);
         }
     }
 }
