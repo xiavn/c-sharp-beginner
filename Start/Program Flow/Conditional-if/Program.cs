69f7d60be4d89d06fdbb240b0e29142603467132
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-24 17:07:54 +0000

    Conditional excercises

--- Start/Program Flow/Conditional-if/Program.cs
+++ Start/Program Flow/Conditional-if/Program.cs
@@ -6,9 +6,18 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 51;
 
             // TODO: if-else 
+            if (theVal == 50){
+                Console.WriteLine("theVal is 50");
+            }
+            else if (theVal >= 51 && theVal <= 60) {
+                Console.WriteLine("theVal is between 51 and 60");
+            }
+            else {
+                Console.WriteLine("theVal is something else");
+            }
 
 
             // -----------------------
@@ -25,7 +34,7 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
-
+            Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
         }
     }
 }
