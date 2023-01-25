a6bd645629ff2b3a629532adf5e5f05bad8a640d
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-25 15:27:38 +0000

    Exceptions

--- Start/Program Flow/Exceptions/Program.cs
+++ Start/Program Flow/Exceptions/Program.cs
@@ -6,13 +6,29 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            int x = 100;
-            int y = 10;
+            int x = 1003;
+            int y = 0;
             int result;
 
-            // TODO: try-catch expressions make error checking easier
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
+            try {
+                // TODO: try-catch expressions make error checking easier
+                if (x > 1000) {
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less.");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e){
+                Console.WriteLine("Whoops!");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e){
+                Console.WriteLine("Sorry! 1000 is the limit!");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("This code always runs.");
+            }
         }
     }
 }
