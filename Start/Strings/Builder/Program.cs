14f2c564f7c7bef0bfea6d50f28c5121908f7380
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-25 17:40:05 +0000

    String Builder Excercises

--- Start/Strings/Builder/Program.cs
+++ Start/Strings/Builder/Program.cs
@@ -11,31 +11,36 @@ namespace Builder
             string[] animals = {"goats", "cats", "pigs"};
 
             // TODO: create a StringBuilder
+            StringBuilder sb = new StringBuilder("Initial string.", 200);
 
             
             // TODO: print some basic stats about the StringBuilder
-
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
 
             // TODO: Add some strings to the builder using Append
-
+            sb.Append(" The quick brown fox ");
+            sb.Append("Jumped over the lazy dog.");
 
             // TODO: AppendLine can append a line ending
-
+            sb.AppendLine();
 
             // TODO: AppendFormat can be used to append formatted strings
-
+            sb.AppendFormat("He did this {0} times.", jumpCount);
+            sb.AppendLine();
 
             // TODO: AppendJoin can iterate over a set of values
-
+            sb.Append("He also jumped over ");
+            sb.AppendJoin(',', animals);
 
             // TODO: Modify the content using Replace
-
+            sb.Replace("fox", "cat");
 
             // TODO: Insert content at any index
-
+            sb.Insert(0, "This is the ");
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
             
             // TODO: Convert to a single string
-
+            Console.WriteLine(sb.ToString());
         }
     }
 }
