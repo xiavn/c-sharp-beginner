2e7529dfe45f5c444794bee175fbb8b9cad37d67
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-25 15:19:25 +0000

    Break/Continue

--- Start/Program Flow/BreakContinue/Program.cs
+++ Start/Program Flow/BreakContinue/Program.cs
@@ -13,13 +13,17 @@ namespace BreakContinue
             {
                 // TODO: The continue statement skips the rest of the loop entirely
                 // and jumps to the next iteration (if there is one)
-
+                if (val >= 20 && val <= 29) {
+                    continue;
+                }
 
                 // print the value
                 Console.WriteLine($"val is currently {val}");
 
                 // TODO: The break statement stops the loop and exits
-
+                if (val >= 40) {
+                    break;
+                }
             }
         }
     }
