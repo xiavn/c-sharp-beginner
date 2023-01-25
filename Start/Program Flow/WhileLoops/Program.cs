d52932d34fb1f6d77c0098f2331d632517484b39
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-25 15:13:35 +0000

    while loops

--- Start/Program Flow/WhileLoops/Program.cs
+++ Start/Program Flow/WhileLoops/Program.cs
@@ -9,14 +9,22 @@ namespace WhileLoops
             string inputStr = "";
 
             // TODO: basic while loop executes while the gate condition is true
-            Console.WriteLine("Basic while() loop:");
-
-            Console.WriteLine();
-
-            // TODO: the do-while loop always executes at least one time
-            // Console.WriteLine("The  do-while() loop:");
+            // Console.WriteLine("Basic while() loop:");
+            // while (inputStr != "exit") {
+            //     inputStr = Console.ReadLine();
+            //     Console.WriteLine("You entered {0}", inputStr);
+            // }
 
             // Console.WriteLine();
+
+            // TODO: the do-while loop always executes at least one time
+            Console.WriteLine("The  do-while() loop:");
+            do {
+                inputStr = Console.ReadLine();
+                Console.WriteLine("You entered {0}", inputStr);
+            } while (inputStr != "exit");
+
+            Console.WriteLine();
         }
     }
 }
