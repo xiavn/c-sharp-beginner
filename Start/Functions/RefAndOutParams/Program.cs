6155cb8400489775d705bcc95252a3cff51285fb
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-26 16:54:15 +0000

    refs and Out

--- Start/Functions/RefAndOutParams/Program.cs
+++ Start/Functions/RefAndOutParams/Program.cs
@@ -5,17 +5,23 @@ namespace RefAndOutParams {
         // TODO: Ordinary value arguments cannot be modified by the function
         // because they are a copy of the original value
         static void TestFunc1(int arg1) {
-
+            arg1 += 10;
             Console.WriteLine($"{arg1}");
         }
 
         // TODO: Arguments that are passed by reference can be modified
         // by the function and reflected back to the caller
-
+        static void TestFunc2(ref int arg1) {
+            arg1 += 10;
+            Console.WriteLine($"{arg1}");
+        }
 
         // TODO: The "out" keyword means that the parameter returns a value and is not
         // used to supply data to the function
-
+        static void PlusTimes(int arg1, int arg2, out int sum, out int product) {
+            sum = arg1 + arg2;
+            product = arg1 * arg2;
+        }
 
         static void Main(string[] args) {
             int val1 = 10;
@@ -27,11 +33,14 @@ namespace RefAndOutParams {
 
             // TODO: Using the "ref" keyword, arguments can be passed by reference
             // which allows the function to modify them
-
+            TestFunc2(ref val1);
+            Console.WriteLine($"{val1}");
 
             // TODO: The "out" keyword can be used to indicate that an argument
             // is intended to return a value and is not an input
-
+            int a, b;
+            PlusTimes(val1, val2, out a, out b);
+            Console.WriteLine($"{a}, {b}");
         }
     }
 }
