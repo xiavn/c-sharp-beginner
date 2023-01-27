b03471545257b22a949fb14ee1c06cc4f7f46b90
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-01-27 17:05:04 +0000

    String Rep Examples

--- Start/Classes/StringRep/Book.cs
+++ Start/Classes/StringRep/Book.cs
@@ -13,10 +13,21 @@ namespace StringRep {
         }
 
         // TODO: The ToString method generates a string represenation of the object
-
+        public override string ToString()
+        {
+            return $"Book: {Name} by {Author}";
+        }
 
         // TODO: ToString can be overloaded to give different format versions
         // Notice that this version is NOT an override function
-
+        public string ToString(char format) {
+            if (format == 'B') {
+                return $"Book: {Name}:{Author}";
+            }
+            if (format == 'F') {
+                return $"Book: {Name} by {Author}, {PageCount} pages";
+            }
+            return ToString();
+        }
     }
 }
