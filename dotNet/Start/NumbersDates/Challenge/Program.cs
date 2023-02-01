0eb96a6c9876f4d955cfee4133e328ae28b3fded
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-01 18:51:04 +0000

    Date Parsing Challenge

---
+++ dotNet/Start/NumbersDates/Challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+string input;
+bool running = true;
+
+do
+{
+    Console.WriteLine($"Enter a date (or 'exit' to quit):");
+    input = Console.ReadLine();
+    try {
+        if (input == "exit") {
+            running = false;
+            break;
+        }
+        DateTime result = DateTime.Parse(input);
+        DateTime today = DateTime.Today;
+        Console.WriteLine($"{result}");
+        
+        
+        int daysDifferent = (result.Date - today.Date).Days;
+        string multiDay = daysDifferent > 1 ? "s" : "";
+        if (daysDifferent == 0) {
+            Console.WriteLine($"That date is today!");
+        } else if (daysDifferent < 0) {
+            Console.WriteLine($"That date went by {Math.Abs(daysDifferent)} day{multiDay} ago!");
+        } else {
+            Console.WriteLine($"That date will be in {daysDifferent} day{multiDay}!");
+        }
+    }
+    catch {
+        Console.WriteLine($"That doesn't seem to be a valid date.");
+    }
+    
+} while (running);
