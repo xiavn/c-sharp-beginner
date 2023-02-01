5d5120dcb7084566f1b337580c0630a8d9919ded
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-01 13:18:50 +0000

    Formatting numbers

--- dotNet/Start/NumbersDates/ParseNumbers/Program.cs
+++ dotNet/Start/NumbersDates/ParseNumbers/Program.cs
@@ -10,8 +10,32 @@ bool result;
 // TODO: The Parse method attempts to parse a string to a number and
 // throws an exception if the parse is unsuccessful
 foreach (string str in NumStrs) {
-
+    try
+    {
+        testfloat = float.Parse(str);
+        Console.WriteLine($"Parsed number is {testfloat}");
+        testint = int.Parse(str);
+        Console.WriteLine($"Parsed number is {testint}");
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine($"Could not parse {str} : {e.Message}");
+    }
 }
 
 // TODO: The TryParse method returns 'true' if the parse is successful
+result = int.TryParse(NumStrs[0], out testint);
+Console.WriteLine($"{result} -- '{NumStrs[0]}' : {testint}");
+result = float.TryParse(NumStrs[0], out testfloat);
+Console.WriteLine($"{result} -- '{NumStrs[0]}' : {testfloat}");
+
+result = int.TryParse(NumStrs[1], out testint);
+Console.WriteLine($"{result} -- '{NumStrs[1]}' : {testint}");
+result = float.TryParse(NumStrs[1], out testfloat);
+Console.WriteLine($"{result} -- '{NumStrs[1]}' : {testfloat}");
+
+result = int.TryParse(NumStrs[1], out testint);
+Console.WriteLine($"{result} -- '{NumStrs[1]}' : {testint}");
+result = float.TryParse(NumStrs[1], out testfloat);
+Console.WriteLine($"{result} -- '{NumStrs[1]}' : {testfloat}");
 
