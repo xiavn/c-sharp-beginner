0a26043447acab29492e45b80f05978e1db815ff
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-02 13:48:23 +0000

    Creating a file

--- dotNet/Start/Files/CreateFiles/Program.cs
+++ dotNet/Start/Files/CreateFiles/Program.cs
@@ -5,6 +5,19 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
-
+// using(StreamWriter sw = File.CreateText(filename)) {
+//     sw.WriteLine("This is a text file");
+// }
 
 // TODO: Determine if a file exists
+bool fileExists = File.Exists(filename);
+Console.WriteLine(fileExists);
+if (fileExists) {
+    File.Delete(filename);
+}
+else {
+    using(StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a text file");
+    }   
+}
+Console.WriteLine(File.Exists(filename));
