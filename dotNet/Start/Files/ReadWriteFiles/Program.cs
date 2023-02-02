5b5b8589515c13d7d5e55309c28f6569c467e7a4
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-02 14:29:25 +0000

    Reading and Writing

--- dotNet/Start/Files/ReadWriteFiles/Program.cs
+++ dotNet/Start/Files/ReadWriteFiles/Program.cs
@@ -1,4 +1,5 @@
-﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+﻿using System.IO;
+// LinkedIn Learning Course .NET Programming with C# by Joe Marini
 // Reading and Writing data from and to files
 
 // Make sure the example file exists
@@ -6,14 +7,21 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-
+    File.WriteAllText(filename, "This is a text file.");
 }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, "This text gets appended to the file.");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using (StreamWriter sw = File.AppendText(filename)) {
+    sw.WriteLine("New line");
+    sw.WriteLine("New line");
+    sw.WriteLine("New line");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
 
