7083d06a7d7bb38c97e8b60e03bf1b9b5cf2e281
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-03 18:26:37 +0000

    Shopping List

--- Collections/Start/Ch2_Basic/Challenge/Program.cs
+++ Collections/Start/Ch2_Basic/Challenge/Program.cs
@@ -7,7 +7,39 @@ namespace Challenge
     {
         static void Main(string[] args)
         {
-            
+            string[] items = {"Oranges", "Apples", "Pears", "Eggs", "Bread", "Butter"};
+            LinkedList<ShoppingItem> shoppingList = new LinkedList<ShoppingItem>();
+            foreach (string name in items) {
+                Random random = new Random();
+                ShoppingItem item = new ShoppingItem(name, (float)random.NextDouble() * 3, random.Next(1,10));
+                shoppingList.AddLast(item);
+            }
+            foreach (ShoppingItem si in shoppingList) {
+                Console.WriteLine($"Item: {si.Name:15}, Price: {si.Price}, Quantity: {si.Quantity}");
+            }
         }
     }
+    public class ShoppingItem {
+
+        float _price;
+
+        public ShoppingItem(string name, float price, int quantity) {
+            Name = name;
+            _price = price;
+            Quantity = quantity;
+        }
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Price {
+            get => $"{_price:C2}";
+        }
+
+        public int Quantity {
+            get;
+            set;
+        }
+    } 
 }
