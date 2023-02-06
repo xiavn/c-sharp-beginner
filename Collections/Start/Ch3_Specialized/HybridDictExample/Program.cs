d2845ac3998bb7b23a78be620c25773cbbe30e1f
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-06 15:50:48 +0000

    Hybrid Dictionary

--- Collections/Start/Ch3_Specialized/HybridDictExample/Program.cs
+++ Collections/Start/Ch3_Specialized/HybridDictExample/Program.cs
@@ -9,22 +9,32 @@ namespace HybridDictExample
         static void Main(string[] args)
         {
             // TODO: Create the hybrid dictionary
-
+            HybridDictionary myHbd = new HybridDictionary(20, false);
 
             // TODO: Add some initial items
-
+            myHbd.Add("item1", "value1");
+            myHbd.Add("item2", "value2");
+            myHbd.Add("item3", "value3");
+            myHbd.Add("item4", "value4");
+            Console.WriteLine($"Dictionary Size: {myHbd.Count}");
+            
 
             // TODO: Contains
-
+            Console.WriteLine(myHbd.Contains("item1"));
+            
 
             // TODO: Remove
-
+            myHbd.Remove("item3");
+            foreach (DictionaryEntry de in myHbd) {
+                Console.WriteLine($"{de.Key}, {de.Value}");
+                
+            }
 
             // TODO: Clear
-
+            myHbd.Clear();
 
             // TODO: Print the number of items
-
+            Console.WriteLine($"Dictionary Size: {myHbd.Count}");
         }
     }
 }
