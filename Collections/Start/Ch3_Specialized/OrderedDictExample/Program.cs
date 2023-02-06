479b3a0995dc7b36b80b0439c346ea29a932cde6
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-06 15:55:00 +0000

    Ordered Dictionary

--- Collections/Start/Ch3_Specialized/OrderedDictExample/Program.cs
+++ Collections/Start/Ch3_Specialized/OrderedDictExample/Program.cs
@@ -12,20 +12,24 @@ namespace OrderedDictExample
             OrderedDictionary mod = new OrderedDictionary();
 
             // TODO: Add some keys and values in a particular order
-
+            mod.Add("item1", "value1");
+            mod.Add("item2", "value2");
+            mod.Add("item3", "value3");
+            mod.Add("item4", "value4");
 
             // TODO: Print the contents of the Dictionary
-
+            PrintDictionary(mod);
 
             // TODO: Remove an existing item and add a new one
-
+            mod.Remove("item3");
+            mod.Add("item5", "value5");
 
             // TODO: Modify the value of an item in the dict
-
+            mod["item2"] = "new value 2";
 
             // TODO: Add one final new item
-
-
+            mod.Add("item6", "value6");
+            PrintDictionary(mod);
         }
 
         static void PrintDictionary(OrderedDictionary theDict)
