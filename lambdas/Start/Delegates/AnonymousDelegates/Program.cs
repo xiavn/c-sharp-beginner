a984abd023c591f5ac76e2fd603b56b8c8b91a86
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-07 14:43:12 +0000

    Anonymous delegates

--- lambdas/Start/Delegates/AnonymousDelegates/Program.cs
+++ lambdas/Start/Delegates/AnonymousDelegates/Program.cs
@@ -9,7 +9,11 @@ namespace AnonymousDelegates
         static void Main(string[] args)
         {
             // TODO: Implement an anonymous delegate
-
+            MyDelegate f = delegate(int arg1, int arg2) {
+                return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine($"The result is: {f(10, 20)}");
+            
         }
     }
 }
