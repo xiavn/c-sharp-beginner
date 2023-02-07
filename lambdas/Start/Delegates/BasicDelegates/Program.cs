5e4c341bcb4599a466fdcf619324ceb5d17b6dc3
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-07 12:10:19 +0000

    basic delegates

--- lambdas/Start/Delegates/BasicDelegates/Program.cs
+++ lambdas/Start/Delegates/BasicDelegates/Program.cs
@@ -3,7 +3,7 @@
 namespace BasicDelegates
 {
     // TODO: declare the delegate type
-
+    public delegate string MyDelegate(int arg1, int arg2);
 
     class MyClass
     {
@@ -18,16 +18,28 @@ namespace BasicDelegates
     class Program
     {
         // TODO: Create functions to serve as delegate implementations
+        static string func1(int a, int b) {
+            return (a + b).ToString();
+        }
 
+        static string func2(int a, int b) {
+            return (a * b).ToString();
+        }
 
         static void Main(string[] args)
         {
             // TODO: exercise each delegate function
-
+            MyDelegate f = func1;
+            Console.WriteLine($"The number from func1 is: {f(10, 20)}");
+            f = func2;
+            Console.WriteLine($"The number from func2 is: {f(10, 20)}");
+            
+            
 
             // TODO: Use an instance function of a class as a delegate
-
-
+            MyClass mc = new MyClass();
+            f = mc.instanceMethod1;
+            Console.WriteLine($"The number from intanceMethod1 is: {f(10, 20)}");
         }
     }
 }
